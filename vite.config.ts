93b6b9f313bbaa52d457af2d76bfbd130d237f6f
André Penha <devmobilekotlinpenha@gmail.com>
2026-07-09 16:48:48 -0300

    Primeiro commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': 'http://localhost:4000'
-    }
-  }
+  base: "https://github.com/andrepenhakotlin/djthesource_eventos_08.07.26",
 });
